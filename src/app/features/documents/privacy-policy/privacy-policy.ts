b97fdd8adea624421c19506f0ae14e2d9227fda6
Mzk-Ali <161448982+Mzk-Ali@users.noreply.github.com>
2026-01-30 15:56:37 +0100

    👌 IMPROVE: Amélioration privacy policy

--- src/app/features/documents/privacy-policy/privacy-policy.ts
+++ src/app/features/documents/privacy-policy/privacy-policy.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MainTitle } from '../../../shared/components/main-title/main-title';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [MainTitle],
+  imports: [MainTitle, RouterLink],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.css',
 })
